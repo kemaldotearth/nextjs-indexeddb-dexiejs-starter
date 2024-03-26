ae5486e6a80f71f93c26c95942b658b105d77e22
kemal <yes@iamerd.xyz>
2024-03-26 22:05:05 +0100

    feat: add example function and getter
    Adds an example function to add some users to the DB we init + a getter for all the users.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,45 @@
+'use client';
+
+import { useStore } from '@/hooks/useStore';
+
 export default function Home() {
+  const { addUser, getAllUsers } = useStore();
+
+  const handleAddUser = async (e: React.SyntheticEvent) => {
+    try {
+      e.preventDefault();
+      const target = e.currentTarget as HTMLFormElement;
+      const username = target.username.value;
+      const email = target.email.value;
+      await addUser(username, email);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  console.log(getAllUsers);
+
   return (
     <main>
       <h1>Next.js + IndexedDB + Dexie.js Starter</h1>
+      <form
+        onSubmit={async (e) => {
+          handleAddUser(e);
+        }}
+      >
+        <input type="text" name="username" placeholder="Username" />
+        <input type="email" name="email" placeholder="Email" />
+        <button type="submit">Add User</button>
+      </form>
+
+      <h2>Users</h2>
+      <ul>
+        {getAllUsers?.map((user, i) => (
+          <li key={i}>
+            <strong>{user.username}</strong> - {user.email}
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
--- hooks/useStore.ts
+++ hooks/useStore.ts
@@ -1,11 +1,22 @@
 import { db } from '@/utils/db';
+import { v4 as uuidv4 } from 'uuid';
+import { useLiveQuery } from 'dexie-react-hooks';
 
 export const useStore = () => {
   const addUser = async (username: string, email: string) => {
-    await db.users.add({ username, email });
+    await db.users.add({
+      id: uuidv4(),
+      username,
+      email,
+    });
   };
+
+  const getAllUsers = useLiveQuery(async () => {
+    return await db.users.toArray();
+  });
 
   return {
     addUser,
+    getAllUsers,
   };
 };
